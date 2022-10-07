d6309e314a3297fbcf363c6ffb73c9c16c3dc7e3
luizfgarcia22 <106934474+luizfgarcia22@users.noreply.github.com>
2022-10-07 09:33:09 -0300

    Add files via upload

---
+++ data/script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+mkdir futebol-01
+cd futebol-01
+mkdir MG SP RJ
+cd MG
+touch cruzeiro atletico america tombense
+cd ../SP
+touch Pameiras sao-paulo santos corinthians
+cd ../RJ
+touch flamengo botafogo vasco fluminense
